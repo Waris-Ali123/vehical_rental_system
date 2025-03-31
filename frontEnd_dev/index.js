1bd1712075bee44dc5848bdcb7fe81f442f757bc
Waris <100395174+Waris-Ali123@users.noreply.github.com>
2025-03-31 23:35:19 +0530

    The api request in index.js is wrapped in try-catch block

--- frontEnd_dev/index.js
+++ frontEnd_dev/index.js
@@ -2,40 +2,43 @@ let loginForm = document.getElementById("loginForm");
 loginForm.addEventListener("submit", login);
 
 async function login(event) {
-  console.log("btn clicked");
   event.preventDefault();
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const response = await fetch(
-    `http://localhost:8080/auth/login?email=${email}&password=${password}`,
-    {
-      method: "GET",
-    }
-  );
+  try {
+    const response = await fetch(
+      `http://localhost:8080/auth/login?email=${email}&password=${password}`,
+      {
+        method: "GET",
+      }
+    );
 
-  if (response.ok) {
-    const data = await response.json();
-    console.log(data);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
 
-    if (data.role == "ADMIN") {
-      localStorage.setItem("admin", JSON.stringify(data));
-      window.location.href = "adminDashboard.html";
+      if (data.role == "ADMIN") {
+        localStorage.setItem("admin", JSON.stringify(data));
+        window.location.href = "adminDashboard.html";
+      } else {
+        localStorage.setItem("user", JSON.stringify(data));
+        window.location.href = "home.html";
+      }
     } else {
-      localStorage.setItem("user", JSON.stringify(data));
-      window.location.href = "home.html";
-    }
-  } else {
-    let isExisting = document.querySelector(".invalidMsg");
-    if (!isExisting) {
-      let invalidMsg = document.createElement("div");
-      invalidMsg.innerText = "Invalid Username or Password";
-      invalidMsg.classList.add("invalidMsg");
+      let isExisting = document.querySelector(".invalidMsg");
+      if (!isExisting) {
+        let invalidMsg = document.createElement("div");
+        invalidMsg.innerText = "Invalid Username or Password";
+        invalidMsg.classList.add("invalidMsg");
 
-      loginForm.insertBefore(invalidMsg, loginForm.firstChild);
-    } else {
-      isExisting.innerHTML = "Invalid Username or Password";
+        loginForm.insertBefore(invalidMsg, loginForm.firstChild);
+      } else {
+        isExisting.innerHTML = "Invalid Username or Password";
+      }
     }
+  } catch (error) {
+    console.log(error);
   }
 }
